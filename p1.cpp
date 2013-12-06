abb6564b37f16a54429ea0b5412481cb650566ef
Alice Zhang <alicezhang05@gmail.com>
2013-12-05 19:31:43 -0500

    generalized the parameters for p1

--- p1.cpp
+++ p1.cpp
@@ -1,11 +1,14 @@
 #include <iostream>
 using namespace std;
 
+#define MAX_NUM 1000
+#define NUM1 3
+#define NUM2 5
 int main()
 {
 	int total = 0;
-	for (int i = 0; i < 1000; i++) {
-	  total += (i % 3 == 0 || i % 5 == 0 ? i : 0);
+	for (int i = 0; i < MAX_NUM; i++) {
+	  total += (i % NUM1 == 0 || i % NUM5 == 0 ? i : 0);
 	}
 	cout << total << "\n";
 	return 0;
